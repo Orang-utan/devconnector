08c126969e9639e3f0b4741d98539ed8d6c2ed01
Daniel Tian <dtian.95@gmail.com>
2019-09-28 02:20:10 -0400

    added models

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000; //PORT is a Heroku env var
-
 const app = express();
+
+// Connect to database
 connectDB();
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.get("/", (req, res) => res.send("API Running"));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
