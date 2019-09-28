3440c907e342e5bd90773a66e1988967ee10d4b3
Daniel Tian <dtian.95@gmail.com>
2019-09-28 02:02:15 -0400

    added server connection

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000; //PORT is a Heroku env var
 
 const app = express();
-
-const PORT = process.env.PORT || 5000; //PORT is a Heroku env var
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
